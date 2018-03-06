d948d85ee41d64fdff7da9b8310d374dfecda9a3
Kalev Alpernas <kalev.alp@gmail.com>
2018-03-06 14:33:13 +0200

    Initial commit - cpp code for forking node.js

---
+++ src/fork.cc
@@ -0,0 +1,32 @@
+// hello.cc
+#include <node.h>
+#include <unistd.h>
+#include <string> 
+
+namespace fork {
+  
+  using v8::FunctionCallbackInfo;
+  using v8::Isolate;
+  using v8::Local;
+  using v8::Object;
+  using v8::Number;
+  using v8::Value;
+  
+  void Method(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+    
+    pid_t pid = fork();
+
+    Local<Number> num = Number::New(isolate, pid);
+    
+    args.GetReturnValue().Set(num);    
+    
+  }
+  
+  void init(Local<Object> exports) {
+    NODE_SET_METHOD(exports, "fork", Method);
+  }
+  
+  NODE_MODULE(hello_world, init)
+  
+}  // namespace fork
