c10812a682a3c5e1b3765d40048212fab61b31d4
Kalev Alpernas <kalev.alp@gmail.com>
2018-03-25 13:57:13 +0300

    Adding pipe to fork module - wip.

--- fork.js
+++ fork.js
@@ -1,4 +1,4 @@
-const {fork, wait} = require('./build/Release/fork');
+const {fork, wait, read, write, pipe} = require('./build/Release/fork');
 
 module.exports.fork = function() {
     pid = fork();
@@ -9,4 +9,23 @@ module.exports.fork = function() {
 module.exports.wait = function() {
     wait();
 }
+
+const pipes = pipe();
+
+if (fork()) { // parent
+    console.log("In parent");
+    message = read(pipes.read_end);
+    console.log("Waiting");
+    wait();
+    console.log("Waited");
+    console.log(message);
+} else { // child
+    console.log("In child");
+    // setTimeout(function() {
+        write(pipes.write_end, "Hello from child");
+        console.log("Sent");      
+    // }, 3000);
+}
+
+
 
--- src/fork.cc
+++ src/fork.cc
@@ -1,8 +1,9 @@
 // hello.cc
 #include <node.h>
 #include <unistd.h>
-#include <string> 
+#include <string.h> 
 #include <sys/wait.h>
+#include <iostream>
 
 namespace nodefork {
   
@@ -11,7 +12,9 @@ namespace nodefork {
   using v8::Local;
   using v8::Object;
   using v8::Number;
+  using v8::String;
   using v8::Value;
+  using v8::Exception;
   
   void Fork(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
@@ -29,10 +32,74 @@ namespace nodefork {
 
     wait(&status);
   }
+
+  void Pipe(const FunctionCallbackInfo<Value>& args) {
+    int pfds[2];
+
+    pipe(pfds);
+    
+    Isolate* isolate = args.GetIsolate();
+
+    Local<Object> obj = Object::New(isolate);
+    
+    obj->Set(String::NewFromUtf8(isolate, "write_end"), Number::New(isolate, pfds[1]));
+    obj->Set(String::NewFromUtf8(isolate, "read_end"),  Number::New(isolate, pfds[0]));
+
+    args.GetReturnValue().Set(obj);
+  }
   
+  void WriteToPipe(const FunctionCallbackInfo<Value>& args) {
+//    Isolate* isolate = args.GetIsolate();
+    
+    int pipe = args[0]->NumberValue();
+    
+    char* message;
+    
+    v8::String::Utf8Value str(args[1]->ToString());
+    message = *str;
+
+    //char* message = *String::Utf8Value(args[1]->ToString());
+    //char message[6] = "abcde";
+    
+    std::cout << message;std::cout << "\n";
+    std::cout << "    ";std::cout << strlen(message);std::cout << "\n";    
+    
+    
+    write(pipe, message, strlen(message));
+    close(pipe);
+
+  }
+
+  void ReadFromPipe(const FunctionCallbackInfo<Value>& args) { 
+    Isolate* isolate = args.GetIsolate();
+    
+    int pipe = args[0]->NumberValue();
+
+    char buf[16];
+
+    // while (read(pipe, &buf, 1) > 0) {
+    //   std::cout << buf;
+    //   //std::cout << "a";
+    // }
+
+    read(pipe, &buf, 16);
+    
+    std::cout << buf;
+    //close(pipe);
+
+    //read(pipe, buf, 20);
+
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate,buf));
+
+  }
+  
+
   void init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "fork", Fork);
     NODE_SET_METHOD(exports, "wait", Wait);
+    NODE_SET_METHOD(exports, "read", ReadFromPipe);
+    NODE_SET_METHOD(exports, "write", WriteToPipe);
+    NODE_SET_METHOD(exports, "pipe", Pipe);
   }
   
   NODE_MODULE(hello_world, init)
