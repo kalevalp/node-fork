affb007225b27d92ffda987c4886502ec0af35df
Kalev Alpernas <kalev.alp@gmail.com>
2018-03-25 16:29:47 +0300

    Added pipe.

--- fork.js
+++ fork.js
@@ -1,4 +1,4 @@
-const {fork, wait, read, write, pipe} = require('./build/Release/fork');
+const {fork, wait, read, write, pipe, close} = require('./build/Release/fork');
 
 module.exports.fork = function() {
     pid = fork();
@@ -6,26 +6,36 @@ module.exports.fork = function() {
     return pid == 0;
 }
 
-module.exports.wait = function() {
-    wait();
-}
+module.exports.wait = wait;
+module.exports.read = read;
+module.exports.write = write;
+module.exports.pipe = pipe;
+module.exports.close = close;
 
-const pipes = pipe();
+// const pipes = pipe();
 
-if (fork()) { // parent
-    console.log("In parent");
-    message = read(pipes.read_end);
-    console.log("Waiting");
-    wait();
-    console.log("Waited");
-    console.log(message);
-} else { // child
-    console.log("In child");
-    // setTimeout(function() {
-        write(pipes.write_end, "Hello from child");
-        console.log("Sent");      
-    // }, 3000);
-}
+// if (fork()) { // parent
+    
+//     close(pipes.write_end);
+    
+//     console.log("In parent");
+//     message = read(pipes.read_end);
+//     console.log("Waiting");
+//     wait();
+
+//     close(pipes.read_end);
+    
+//     console.log("Waited");
+//     console.log(message);
+// } else { // child
+//     close(pipes.read_end);
+    
+//     console.log("In child");
+//     write(pipes.write_end, "Hello from child");
+//     console.log("Sent");      
+
+//     close(pipes.write_end);
+// }
 
 
 
--- src/fork.cc
+++ src/fork.cc
@@ -49,8 +49,6 @@ namespace nodefork {
   }
   
   void WriteToPipe(const FunctionCallbackInfo<Value>& args) {
-//    Isolate* isolate = args.GetIsolate();
-    
     int pipe = args[0]->NumberValue();
     
     char* message;
@@ -58,15 +56,7 @@ namespace nodefork {
     v8::String::Utf8Value str(args[1]->ToString());
     message = *str;
 
-    //char* message = *String::Utf8Value(args[1]->ToString());
-    //char message[6] = "abcde";
-    
-    std::cout << message;std::cout << "\n";
-    std::cout << "    ";std::cout << strlen(message);std::cout << "\n";    
-    
-    
     write(pipe, message, strlen(message));
-    close(pipe);
 
   }
 
@@ -75,24 +65,21 @@ namespace nodefork {
     
     int pipe = args[0]->NumberValue();
 
-    char buf[16];
+    char buf;
+    std::string str;
 
-    // while (read(pipe, &buf, 1) > 0) {
-    //   std::cout << buf;
-    //   //std::cout << "a";
-    // }
+    while (read(pipe, &buf, 1) > 0) {
+      str += buf;
+    }
 
-    read(pipe, &buf, 16);
-    
-    std::cout << buf;
-    //close(pipe);
-
-    //read(pipe, buf, 20);
-
-    args.GetReturnValue().Set(String::NewFromUtf8(isolate,buf));
-
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate,str.c_str()));//buf));
   }
-  
+
+  void Close(const FunctionCallbackInfo<Value>& args) { 
+    int pipe = args[0]->NumberValue();
+
+    close(pipe);
+  }
 
   void init(Local<Object> exports) {
     NODE_SET_METHOD(exports, "fork", Fork);
@@ -100,6 +87,7 @@ namespace nodefork {
     NODE_SET_METHOD(exports, "read", ReadFromPipe);
     NODE_SET_METHOD(exports, "write", WriteToPipe);
     NODE_SET_METHOD(exports, "pipe", Pipe);
+    NODE_SET_METHOD(exports, "close", Close);
   }
   
   NODE_MODULE(hello_world, init)
