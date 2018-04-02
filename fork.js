cb1438a296fa960aeaafcb5e8248475bd88fe344
Kalev Alpernas <kalev.alp@gmail.com>
2018-04-02 09:19:53 +0300

    Test flag in fork module.

--- fork.js
+++ fork.js
@@ -12,30 +12,41 @@ module.exports.write = write;
 module.exports.pipe = pipe;
 module.exports.close = close;
 
-// const pipes = pipe();
+if (process.argv[2] === "test") {
+    const before_pipes = Date.now();
+    const pipes = pipe();
 
-// if (fork()) { // parent
-    
-//     close(pipes.write_end);
-    
-//     console.log("In parent");
-//     message = read(pipes.read_end);
-//     console.log("Waiting");
-//     wait();
+    const before_fork = Date.now();
 
-//     close(pipes.read_end);
-    
-//     console.log("Waited");
-//     console.log(message);
-// } else { // child
-//     close(pipes.read_end);
-    
-//     console.log("In child");
-//     write(pipes.write_end, "Hello from child");
-//     console.log("Sent");      
+    if (fork()) { // parent
+        console.log("Fork took ", Date.now()-before_fork);
+        console.log("Pipe took", before_fork-before_pipes)
+        
 
-//     close(pipes.write_end);
-// }
+        close(pipes.write_end);
+        
+        console.log("In parent");
+        message = read(pipes.read_end);
+        console.log("After read: ", Date.now());
+        console.log("Waiting");
+        wait();
+
+        close(pipes.read_end);
+        
+        console.log("Waited");
+        console.log(message);
+    } else { // child
+        close(pipes.read_end);
+        
+        console.log("In child");
+        console.log("Before write: ", Date.now());
+
+        write(pipes.write_end, "Hello from child");
+        console.log("Sent");      
+
+        close(pipes.write_end);
+    }
 
 
 
+}
